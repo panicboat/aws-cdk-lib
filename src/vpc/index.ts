8d7955c6aab1626aba492ea1363023ce1edac728
panicboat <panicboat@gmail.com>
2023-03-04 14:34:43 +0900

    add vpc

---
+++ src/vpc/index.ts
@@ -0,0 +1,24 @@
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
+import { Construct } from 'constructs';
+
+interface IVpc {
+}
+export interface Props {
+  cidr?: string
+  subnetConfiguration?: ec2.VpcProps['subnetConfiguration']
+}
+export class Vpc extends Construct implements IVpc {
+
+  constructor(scope: Construct, id: string, props: Props) {
+    super(scope, id);
+
+  }
+
+  private vpc(scope: Construct, id: string, props: Props) {
+    const vpc = new ec2.Vpc(scope, `Vpc-${id}`, {
+      vpcName: id,
+      cidr: props.cidr,
+      subnetConfiguration: props.subnetConfiguration,
+    })
+  }
+}
