77fc668f75a25ed2bc2ec9b1e52192aea251daed
panicboat <panicboat@gmail.com>
2023-03-05 22:35:24 +0900

    Update index.ts

--- src/vpc/index.ts
+++ src/vpc/index.ts
@@ -4,12 +4,19 @@ import { Construct } from 'constructs';
 interface IVpc {
 }
 export interface Props {
+  availabilityZones?: string[]
   cidr?: string
   maxAzs?: number
   subnetConfiguration?: ec2.VpcProps['subnetConfiguration']
 }
 export class Vpc extends Construct implements IVpc {
 
+  /**
+ * Vpc creates a VPC that spans a whole region.
+ * It will automatically divide the provided VPC CIDR range, and create public and private subnets per Availability Zone.
+ * Network routing for the public subnets will be configured to allow outbound access directly via an Internet Gateway.
+ * Network routing for the private subnets will be configured to allow outbound access via a set of resilient NAT Gateways (one per AZ).
+ */
   constructor(scope: Construct, id: string, props: Props) {
     super(scope, id);
     this.vpc(scope, id, props)
@@ -19,7 +26,7 @@ export class Vpc extends Construct implements IVpc {
     const vpc = new ec2.Vpc(scope, `Vpc-${id}`, {
       vpcName: id,
       cidr: props.cidr,
-      maxAzs: props.maxAzs,
+      availabilityZones: props.availabilityZones,
       subnetConfiguration: props.subnetConfiguration,
       gatewayEndpoints: {
         s3: {
